7dea20668375ecc7c51933ea738d2a1c41acb079
pablo <pabloccsanchez@gmail.com>
2026-07-02 14:32:37 -0500

    feat: add weekly service schedule

--- src/lib/components/DayCard.svelte
+++ src/lib/components/DayCard.svelte
@@ -1,41 +1,46 @@
+<script>
+  let { service } = $props()
+  const { day, name, date, team } = $derived(service)
+  const { LIDER, APOYO } = $derived(team)
+
+  const INSTRUMENTS = ['PIANO', 'BATERIA', 'GUITARRA', 'BAJO']
+</script>
+
 <article class="flex flex-col text-black h-auto gap-5 bg-white p-8 rounded-2xl shrink-0 w-full snap-center">
-  <div class="ml-auto text-right">
+  <div class="ml-auto text-right flex flex-col gap-0">
     <span class="text-sm text-black/80 font-semibold">
       vocalistas
     </span>
     <ul class="text-lg -space-y-1.5">
-      <li class="font-bold">Mariana Lopez</li>
-      <li>Sara Ramos</li>
-      <li>Carolina Ramos</li>
+      <li class="font-bold">{LIDER}</li>
+      {#each APOYO as vocalist}
+        <li>{vocalist}</li>
+      {/each}
     </ul>
   </div>
 
-  <div>
+  <div class="flex flex-col gap-0">
     <span class="text-sm text-black/80 font-semibold">
       instrumentos
     </span>
     <ul class="text-lg -space-y-1.5">
-      <li>
-        <span class="text-black/80 italic">piano.</span>
-        Santiago Arias
-      </li>
-      <li>
-        <span class="text-black/80 italic">guitarra.</span>
-        Josué Mejia
-      </li>
-      <li>
-        <span class="text-black/80 italic">batería.</span>
-        Mateo Velez
-      </li>
+      {#each INSTRUMENTS as instrumet}
+        {#if team[instrumet]}
+          <li>
+            <span class="text-black/80 italic">{instrumet.toLowerCase()}.</span>
+            {team[instrumet]}
+          </li>
+        {/if}
+      {/each}
     </ul>
   </div>
 
-  <div class="flex flex-col gap-2 font-serif">
+  <div class="flex flex-col gap-1 font-serif">
     <h2 class="text-5xl">
-      JUEVES 25
+      {day} {date}
     </h2>
     <h3 class="text-2xl text-black/80">
-      ORACIÓN Y DOCTRINA
+      {name}
     </h3>
   </div>
 </article>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,14 +3,17 @@
   import DayCard from './DayCard.svelte';
   import MainCard from './MainCard.svelte';
   import Hero from './MainCard.svelte';
+  import data from '$lib/mock/data.json'
+
+  const { title, services } = data
 </script>
 
-<section class="flex flex-col justify-between h-full bg-neutral-950 text-white p-10 py-16">
+<section class="flex flex-col justify-between h-full bg-neutral-950 text-white p-10 py-18">
   <div class="flex flex-col items-center gap-1.5">
     <span
       class="bg-neutral-400 text-neutral-800/90 px-2.5 rounded-md text-lg font-sans font-bold tracking-tight"
     >
-      22 de febrero - 1 de marzo
+      {title}
     </span>
     <a class="text-neutral-400/90 text-lg underline" href="https://youtube.com">
       Eres administrador? Ingresa aquí
@@ -20,7 +23,9 @@
   <div class="flex flex-col gap-5">
     <div class="flex w-full overflow-x-scroll snap-x snap-mandatory scrollbar-none gap-1 items-end">
       <MainCard />
-      <DayCard />
+      {#each services as service}
+        <DayCard service={service} />
+      {/each}
     </div>
 
     <aside class="w-full flex gap-2">
