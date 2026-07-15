e25a06b276c74f7911f2f8fdd154e8ef578ec479
pablo <pabloccsanchez@gmail.com>
2026-07-15 18:35:42 -0500

    feat: add view transition handling on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
   import "../app.css";
 
+  import { onNavigate } from "$app/navigation";
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return
+    return new Promise(resolve => {
+      document.startViewTransition(async () => {
+        resolve()
+        await navigation.complete
+      })
+    })
+  })
+
   let { children } = $props();
 </script>
 
@@ -9,7 +21,6 @@
 </svelte:head>
 
 <main>
-	
 
 </main>
 
